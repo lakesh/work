205a043c1cdfc5a34f255e1a38bd12de2819decf
Lakesh Kansakar <lakesh.kansakar@gmail.com>
2012-04-16 09:04:08 -0400

    Modified the training Added first revision of temporal and spatial correlation feature

--- train.m
+++ train.m
@@ -3,6 +3,7 @@
 
 %load the MISR,MODIS, AERONET collocated data
 load('/work/collocated_MISR_MODIS_AERONET.mat');
+%load('/work/collocatedData_MISR_MODIS_AERONET_withMissingAttributes.mat');
 
 [row column] = size(collocatedData);
 
@@ -52,6 +53,7 @@ end
 row = 9;
 column = 13;
 numberOfDays = 365;
+days = 365;
 
 N = row*column;
 
@@ -83,7 +85,7 @@ sparseIndexX = zeros(N,1);
 sparseIndexY = zeros(N,1);
 sparseIndexValue = zeros(N,1);
 
-
+index = 1;
 for i=1:N
     x = linearMatrix(i,1);
     y = linearMatrix(i,2);
@@ -99,16 +101,13 @@ for i=1:N
             
             % For the four corner elements number of neighbours = 2
             if i == 1 || i == N || (x == 1 && y == column) || (x == row && y == 1)
-                %sparseIndexValue(index,1) = 2*beta1 + beta2;
                 sparseIndexValue(index,1) = 2;
             elseif x >=2 && y >= 2 && x < row && y < column
                 % For the elements in the middle number of neighbours = 4
-                %sparseIndexValue(index,1) = 4*beta1 + beta2;
                 sparseIndexValue(index,1) = 4;
             else
                 % For the elements at the edge but not the corner
                 % number of neighbours = 3
-                %sparseIndexValue(index,1) = 3*beta1 + beta2;
                 sparseIndexValue(index,1) = 3;
             end
             index = index + 1;
@@ -117,7 +116,6 @@ for i=1:N
             if (abs(x-x1) == 1 && abs(y-y1) == 0) || (abs(x-x1) == 0 && abs(y-y1) == 1)
                 sparseIndexX(index,1) = i;
                 sparseIndexY(index,1) = j;
-                %sparseIndexValue(index,1) = -1*beta1;
                 sparseIndexValue(index,1) = -1;
                 index = index + 1;
             end
@@ -159,51 +157,35 @@ QSpatial = sparse(sparseIndexX_WholeYear', sparseIndexY_WholeYear', sparseIndexV
 sparseIndexX = zeros(N,1);
 sparseIndexY = zeros(N,1);
 sparseIndexValue = zeros(N,1);
+index = 1;
 
-
-for i=1:N
-    x = linearMatrix(i,1);
-    y = linearMatrix(i,2);
-    for j=1:N
-        x1 = linearMatrix(j,1);
-        y1 = linearMatrix(j,2);
-        
-        if(i == j)
-            %diagonal element
-            sparseIndexX(index,1) = i;
-            sparseIndexY(index,1) = j;
-            
-            if i == 1 || i == N || (x == 1 && y == column) || (x == row && y == 1)
-                sparseIndexValue(index,1) = beta2;
-            elseif x >=2 && y >= 2 && x < row && y < column
-                
-                sparseIndexValue(index,1) = beta2;
-            else    
-                sparseIndexValue(index,1) = beta2;
-            end
-            index = index + 1;
-        end
-    end
+for i=1:N*days
+    %diagonal element
+    sparseIndexX(index,1) = i;
+    sparseIndexY(index,1) = i;
+    sparseIndexValue(index,1) = 1;
+    index = index + 1;
 end
 
 
 [m n] = size(sparseIndexX);
 
-sparseIndexX_WholeYear = zeros(m*365,1);
-sparseIndexY_WholeYear = zeros(m*365,1);
-sparseIndexValue_WholeYear = zeros(m*365,1);
+sparseIndexX_WholeYear = zeros(m,1);
+sparseIndexY_WholeYear = zeros(m,1);
+sparseIndexValue_WholeYear = zeros(m,1);
 
 sparseIndexX_WholeYear(1:m,1)=sparseIndexX(1:m,1);
 sparseIndexY_WholeYear(1:m,1)=sparseIndexY(1:m,1);
 sparseIndexValue_WholeYear(1:m,1)=sparseIndexValue(1:m,1);
 
+
 %Populate the remaining beta2 values
-index = m*365+1;
+index = m+1;
 j = N+1;
 for i=1:N*364;
     sparseIndexX_WholeYear(index,1) = i;
     sparseIndexY_WholeYear(index,1) = j;
-    sparseIndexValue_WholeYear(index,1) = -beta2;
+    sparseIndexValue_WholeYear(index,1) = -1;
     j=j+1;
     index = index + 1;
 end
@@ -212,7 +194,7 @@ i=1;
 for j=N+1:N*365
     sparseIndexX_WholeYear(index,1) = j;
     sparseIndexY_WholeYear(index,1) = i;
-    sparseIndexValue_WholeYear(index,1) = -beta2;
+    sparseIndexValue_WholeYear(index,1) = -1;
     i=i+1;
     index = index + 1;
 end
@@ -221,20 +203,45 @@ end
 [m n] = size(sparseIndexX_WholeYear);
 for i=1:m
     if (sparseIndexX_WholeYear(i,1) == sparseIndexY_WholeYear(i,1)) && sparseIndexX_WholeYear(i,1) > N && sparseIndexX_WholeYear(i,1) <= N*364
-        sparseIndexValue_WholeYear(i,1) = sparseIndexValue_WholeYear(i,1) + beta2;
+        sparseIndexValue_WholeYear(i,1) = sparseIndexValue_WholeYear(i,1) + 1;
     end
 end
 
 QTemporal = sparse(sparseIndexX_WholeYear', sparseIndexY_WholeYear', sparseIndexValue_WholeYear', N*365, N*365);
 
 %%%%%%%%%%%%%%%% End calculating QTemporal %%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-
-
+clear sparseIndexX sparseIndexY sparseIndexValue sparseIndexValue_WholeYear sparseIndexX_WholeYear sparseIndexY_WholeYear linearMatrix
 
 
 
 [row column] = size(collocatedData);
+
+%%%%Sort the QTemporal and QSpatial matrix based on collocated data
+%%%%%%%%%%%%%%%%
+
+%Get the index for the collocated points to sort the Q matrix
+collocatedIndex = zeros(row,1);
+index = 1;
+sizeX = 13;
+sizeY = 9;
+for i=1:row
+    data = collocatedData(index,:);
+    collocatedIndex(index,1) = (data(1,8) - 1)*sizeX*sizeY + (data(1,9)-1)*sizeX + data(1,10);
+    index = index + 1;
+end
+
+%Calculate QLL_Spatial
+QLL_Spatial = QSpatial(collocatedIndex,:);
+QLL_Spatial = QLL_Spatial(:,collocatedIndex);
+
+%Calculate QLL_Temporal
+QLL_Temporal = QTemporal(collocatedIndex,:);
+QLL_Temporal = QLL_Temporal(:,collocatedIndex);
+
+%%%%%%%% End of sorting %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+
 
 
 iterate = true;
@@ -254,46 +261,6 @@ while iterate == true
     sparseIndexY_Q1 = zeros(row,1);
     sparseIndexValue_Q1 = zeros(row,1);
 
-    sparseIndexX_Q2 = zeros(1,1);
-    sparseIndexY_Q2 = zeros(1,1);
-    sparseIndexValue_Q2 = zeros(1,1);
-
-    index = 1;
-
-    %For spatial correlation (Q2)
-    %Populate the -beta1 values
-    for i=1:row
-        for j=1:row
-            if i ~= j && collocatedData(i,8) == collocatedData(j,8) && (abs(collocatedData(i,9)-collocatedData(j,9)) == 1 && abs(collocatedData(i,10)-collocatedData(j,10)) == 0) && (abs(collocatedData(i,10)-collocatedData(j,10)) == 1 && abs(collocatedData(i,9)-collocatedData(j,9)) == 0)
-                sparseIndexX_Q2(index,1) = i;
-                sparseIndexY_Q2(index,1) = j;
-                sparseIndexValue_Q2(index,1) = -1;
-                index = index+1;
-            end
-        end
-    end
-    Q2 = [];
-
-    %check if there is any correlation
-    if all(sparseIndexX_Q2) == 1
-        %If yes initialize the Q2 matrix with the correlated data
-        Q2= sparse(sparseIndexX_Q2', sparseIndexY_Q2', sparseIndexValue_Q2', row, row);
-        %In Q2 matrix the absolute sum of the non diagonal elements == the value of the
-        %diagonal element so initialize the diagonal element with that value
-        for i=1:row
-            position = find(sparseIndexX_Q2(:,1) == i);
-            [row1 column1] = size(position);
-            sparseIndexX_Q2(index,1) = i;
-            sparseIndexY_Q2(index,1) = i;
-            sparseIndexValue_Q2(index,1) = row1;
-            index = index+1;
-        end
-    else
-        Q2 = sparse(row, row);
-    end
-
-
-
     %For Q1 matrix
     %Populate the diagonal values alpha1*delta1(xi) + alpha2*delta2(xi) +
     %alpha3
@@ -308,40 +275,45 @@ while iterate == true
     end
     Q1 = sparse(sparseIndexX_Q1', sparseIndexY_Q1', sparseIndexValue_Q1', row, row);
 
+    Q2 = QLL_Spatial;
+    Q3 = QLL_Temporal;
+    
     %The Q matrix
-    Q=Q1+Q2;
-
+    Q=Q1+Q2+Q3;
 
 
 
     %Learning rate
-    n = 0.01;
+    n = 0.001;
 
     %Q_full = full(Q);
     %calculate the derivative
     delta_log_alpha1 = alpha1* (-sumMISR + 1/2 * trace(Q\E1) - X(:,1)'*(Q\b) + b'* (Q\(E1*(Q\b))) - b'*(Q\X(:,1)) - (alpha1));
     delta_log_alpha2 = alpha2* (-sumMODIS + 1/2 * trace(Q\E2) - X(:,2)'*(Q\b) + b'* (Q\E2*(Q\b)) - b'*(Q\X(:,2)) - (alpha2));
-    delta_log_beta2 = beta2 * (beta2*collocatedData(:,5)'*Q2*collocatedData(:,5) + 1/2*trace(Q\Q2) - b'* (Q\(Q2*(Q\b))));
+    delta_log_beta1 = beta1 * (-beta1*collocatedData(:,5)'*Q2*collocatedData(:,5) + 1/2*trace(Q\Q2) - b'* (Q\(Q2*(Q\b)))- (beta1));
+    delta_log_beta2 = beta2 * (-beta2*collocatedData(:,5)'*Q3*collocatedData(:,5) + 1/2*trace(Q\Q3) - b'* (Q\(Q3*(Q\b)))- (beta2));
 
     %Update the parameters
     log_alpha1 = log(alpha1) + n * delta_log_alpha1;
     log_alpha2 = log(alpha2) + n * delta_log_alpha2;
+    log_beta1 = log(beta1) + n * delta_log_beta1;
     log_beta2 = log(beta2) + n * delta_log_beta2;
 
     alpha1_new = exp(log_alpha1);
     alpha2_new = exp(log_alpha2);
+    beta1_new = exp(log_beta1);
     beta2_new = exp(log_beta2);
     
-    difference = abs(alpha1_new-alpha1) + abs(alpha2_new-alpha2) + abs(beta2_new-beta2);
+    difference = abs(alpha1_new-alpha1) + abs(alpha2_new-alpha2) + abs(beta1_new-beta1) + abs(beta2_new-beta2);
     
     alpha1 = alpha1_new;
     alpha2 = alpha2_new;
+    beta1 = beta1_new;
     beta2 = beta2_new;
     
-    if difference <= 0.0001
+    if difference <= 0.001
         iterate = false;
     end
-    %traceLog(traceIndex,1) = difference;
-    %traceIndex = traceIndex + 1;
+    
     disp(difference);
 end
